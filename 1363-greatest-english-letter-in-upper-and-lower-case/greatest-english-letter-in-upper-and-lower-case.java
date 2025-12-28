bd0658a4ea45d25ea2fc2363c5269e66272fb816
Swaroop kosaraju <swaroopkosaraju28@gmail.com>
2025-12-28 21:54:15 +0530

    Time: 10 ms (18.11%) | Memory: 43.6 MB (25.56%) - LeetSync

---
+++ 1363-greatest-english-letter-in-upper-and-lower-case/greatest-english-letter-in-upper-and-lower-case.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String greatestLetter(String word) {
+        HashSet<Character> set = new HashSet<>();
+        for(int i=0;i<word.length();i++){
+            set.add(word.charAt(i));
+        }
+        String s = "";
+        for(char ch = 'z';ch>='a';ch--){
+            if(set.contains(ch) && set.contains(Character.toUpperCase(ch))){
+                return String.valueOf(Character.toUpperCase(ch));
+            }
+        }
+        return s;
+    }
+}
