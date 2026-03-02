eb13924c2769cc65bf0990aad0decc8b1b39eef8
Swaroop kosaraju <swaroopkosaraju28@gmail.com>
2026-03-02 09:52:39 +0530

    Time: 6 ms (32.21%) | Memory: 46.8 MB (88.05%) - LeetSync

---
+++ 3226-minimum-number-game/minimum-number-game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] numberGame(int[] nums) {
+        Arrays.sort(nums);
+        int[] arr = new int[nums.length];
+        int index = 0;
+        for(int i=0;i<nums.length;i=i+2){
+            arr[index++] = nums[i+1];
+            arr[index++] = nums[i];
+
+        }
+        return arr;
+    }
+}
