cf167275ee5a981103b80ccaebbfccee028bfba0
Swaroop kosaraju <swaroopkosaraju28@gmail.com>
2026-08-14 19:28:14 +0530

    Time: 8 ms (67.23%) | Memory: 48.3 MB (16.40%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -3,29 +3,28 @@ class Solution {
         int low = 1;
         int high = max(piles);
         while(low <= high){
-            int mid = low+(high-low)/2;
-            long time = caltime(piles,mid);
+            int mid = low + (high-low)/2;
+            long time = calcu(piles,mid);
             if(time <= h){
-                high = mid - 1;
+                high = mid-1;
             }else{
-                low = mid + 1;
+                low = mid +1;
             }
         }
         return low;
     }
     private int max(int[] piles){
-        int m = 0;
-        for(int num : piles){
-            m = Math.max(m,num);
+        int m=0;
+        for(int n : piles){
+            m = Math.max(m,n);
         }
         return m;
-
     }
-    private long caltime(int[] piles,int mid){
-        long t = 0;
-        for(int n : piles){
-            t += ((long)n + mid - 1) / mid;
+    private long calcu(int[] piles,int mid){
+        long t =0;
+        for(int p : piles){
+        t += ((long)p+mid-1)/mid;
         }
-        return t;
+    return t;
     }
 }
