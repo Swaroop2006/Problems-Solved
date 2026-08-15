9fd18d22cd1dfed85a73732f2d2e9ad5352f673d
Swaroop kosaraju <swaroopkosaraju28@gmail.com>
2026-08-15 13:24:28 +0530

    Time: 0 ms (100.00%) | Memory: 48.5 MB (31.71%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -4,17 +4,15 @@ class Solution {
         int low = 0;
         int high = n-1;
         while(low <= high){
-            int mid = low + (high-low)/2;
+            int mid = low + (high - low)/2;
             if(nums[mid] == target){
                 return mid;
-            }
-            else if(target > nums[mid]){
-                low = mid + 1;
-            }else{
+            }else if(nums[mid] > target){
                 high = mid -1;
+            }else{
+                low = mid + 1;    
             }
         }
         return -1;
-    
     }
 }
